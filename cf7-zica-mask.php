05571bd8fcf88e8422e7a09ca3558bedc5112342
Jano Sousa <sincromaster@users.noreply.github.com>
2018-01-22 12:53:42 -0200

    Update cf7-zica-mask.php

--- cf7-zica-mask.php
+++ cf7-zica-mask.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 
 function zica_mask_enqueue_masked_input(){
  
-      if( is_page('solicite-um-orcamento') )  //vai carregar somente na pagina Contato.
+      if( is_page('contato') )  //vai carregar somente na pagina Contato.
             wp_enqueue_script('masked-input', plugin_dir_url( __FILE__ ) . '/js/jquery.mask.min.js', array('jquery'), '1.0.0');
  
 }
@@ -21,7 +21,7 @@ add_action('wp_enqueue_scripts', 'zica_mask_enqueue_masked_input');
 add_action('wp_footer', 'zica_mask_activate_masked_input');
  
 function zica_mask_activate_masked_input(){
-   if( is_page('solicite-um-orcamento') ){ //mais uma vez, feito isso, o script abaixo só aparecerá na página Contato.
+   if( is_page('contato') ){ //mais uma vez, feito isso, o script abaixo só aparecerá na página Contato.
 ?>
  
          <script type="text/javascript">
